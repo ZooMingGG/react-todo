affe5f2a727c0ab7553204b226693640cb7463db
Volodymyr Lukashevych <lukashevych.volodymyr@gmail.com>
2024-01-15 13:49:01 +0200

    add test todo

--- src/App.js
+++ src/App.js
@@ -1,75 +1,78 @@
-import React from 'react'
+import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 import Task from './Components/Task/Task';
 import TaskInput from './Components/TaskInput/TaskInput';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       tasks: [
-        {id: 0, title: 'Learn React', done: true},
-        {id: 1, title: 'Learn Angular', done: true},
-        {id: 2, title: 'Learn Vue', done: false}
-      ]
-    }
+        { id: 0, title: 'Learn React', done: true },
+        { id: 1, title: 'Learn Angular', done: true },
+        { id: 2, title: 'Learn Vue', done: false },
+      ],
+    };
   }
 
-  changeHandler = id => {
+  // TODO: test TODO
+
+  changeHandler = (id) => {
     const tasks = [...this.state.tasks];
-    tasks.forEach(task => {
+    tasks.forEach((task) => {
       if (task.id === id) {
         task.done = !task.done;
       }
-    })
+    });
 
-    this.setState({tasks});
-  }
+    this.setState({ tasks });
+  };
 
-  addTaskHandler = title => {
+  addTaskHandler = (title) => {
     if (title) {
       const tasks = [...this.state.tasks];
 
       tasks.unshift({
         id: this.state.tasks.length ? this.state.tasks.length : 0,
         title: title,
-        done: false
+        done: false,
       });
 
-      this.setState({tasks});
+      this.setState({ tasks });
     }
-  }
+  };
 
-  deleteTaskHandler = id => {
-    const tasks = [...this.state.tasks.filter(task => task.id !== id)];
+  deleteTaskHandler = (id) => {
+    const tasks = [...this.state.tasks.filter((task) => task.id !== id)];
 
-    this.setState({tasks});
-  }
+    this.setState({ tasks });
+  };
 
   render() {
-    const finishedTasks = this.state.tasks.filter(task => task.done);
-    const unfinishedTasks = this.state.tasks.filter(task => !task.done);
+    const finishedTasks = this.state.tasks.filter((task) => task.done);
+    const unfinishedTasks = this.state.tasks.filter((task) => !task.done);
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <div className="active-tasks">Active Tasks: {unfinishedTasks.length}</div>
+        <div className="active-tasks">
+          Active Tasks: {unfinishedTasks.length}
+        </div>
         <TaskInput addTaskHandler={this.addTaskHandler} />
-        { [...unfinishedTasks, ...finishedTasks].map((task, index) => {
+        {[...unfinishedTasks, ...finishedTasks].map((task, index) => {
           return (
-            <Task 
+            <Task
               key={index}
               checked={task.done}
               changeHandler={this.changeHandler.bind(this, task.id)}
               deleteTaskHandler={this.deleteTaskHandler.bind(this, task.id)}
-              title={task.title} 
+              title={task.title}
             />
           );
-        }) }
+        })}
       </div>
     );
   }
